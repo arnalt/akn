ff9414a3d329e1f40b9664c72dd8de30f8f3527a
aral <altenhofer@web.de>
2013-04-17 15:11:21 +0200

    changes for empty works in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   before_filter :check_period, only: :output
+  before_filter :check_work, only: :output
 
 
   def input
@@ -10,14 +11,10 @@ class ReportsController < ApplicationController
   end
 
   def output
-    @clientname =params[:Clientname]
-    start_at = (params[:period_begin].to_s).to_date
-    end_at = (params[:period_end].to_s).to_date
-    @works = current_user.works.build_report(start_at, end_at, @clientname).order("date")
-     @total_std = current_user.works.build_report(start_at, end_at, @clientname).sum(:working_hours).to_f
+    @total_std = current_user.works.build_report(@start_at, @end_at, @clientname).sum(:working_hours).to_f
     @username = current_user.user_name
     @total_tage = @total_std / 8.0
-    @akt_monat = I18n.t("date.month_names")[start_at.to_s[6, 2].to_i]
+    @akt_monat = I18n.t("date.month_names")[@start_at.to_s[6, 2].to_i]
     @bar_chart=  (@works.first.date..@works.last.date).map { |date| Work.total_on(date,@works).to_f}
     @xdata =  (@works.first.date..@works.last.date).each.map { |d| d.to_s[8, 2]}
     @w_avg = current_user.works.get_avg(start_at, end_at,@clientname)
@@ -39,6 +36,17 @@ class ReportsController < ApplicationController
     end
   end
 
+  def check_work
+    @clientname =params[:Clientname]
+    @start_at = (params[:period_begin].to_s).to_date
+    @end_at = (params[:period_end].to_s).to_date
+    @works = current_user.works.build_report(@start_at, @end_at, @clientname).order("date")
+    if @works.empty?
+      flash[:error] = I18n.t("messages.empty_work")
+      redirect_to works_path
+    end
+
+  end
 
 end
 
