684a047aed479a5adf2898bd6a0046e8d7a0b714
aral <altenhofer@web.de>
2013-05-03 10:28:44 +0200

    seeds changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,12 @@ users =User.create([
 clients=Client.create([
  {name: 'Testclient', project: 'Testproject', created_at: '2013-03-22 12:14:13.258674', updated_at: '2013-03-22 12:14:13.258674'} ])
 
+tasks=Task.create([
+                          {name: 'Task#1000', description: 'Test', created_at: '2013-03-22 12:14:13.258674', updated_at: '2013-03-22 12:14:13.258674', client_id: clients.first.id} ])
 Work.create([
  {date: '2013-04-10', week: 15, day: 'Mittwoch', start_at: '2000-01-01 07:30:00.000000', end_at: '2000-01-01 16:00:00.000000',
   pause: '2000-01-01 00:30:00.000000', working_hours: 8.0, description: 'Test', created_at: '2013-04-09 08:41:27.812524',
-  updated_at:'2013-04-09 08:41:27.812524', user_id: users.first.id, client_id: clients.first.id } ])
+  updated_at:'2013-04-09 08:41:27.812524', user_id: users.first.id, client_id: clients.first.id, task_id: tasks.first.id } ])
 
 
 
