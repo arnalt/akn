e82735a4390d81487be30966e6333b771c85e1d1
aral <altenhofer@web.de>
2013-05-04 09:49:22 +0200

    solve error with empty works

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -42,20 +42,23 @@ class ReportsController < ApplicationController
     else
       @client_id = Client.get_client_id(params[:clientname]).at(0).id
       @works = current_user.works.build_report(@start_at, @end_at, @client_id).order("date")
-      @total_std = current_user.works.build_report(@start_at, @end_at, @client_id).sum(:working_hours).to_f
-      @username = current_user.user_name
-      @total_tage = @total_std / 8.0
-      @akt_monat = I18n.t("date.month_names")[@start_at.to_s[6, 2].to_i]
-      @bar_chart= (@works.first.date..@works.last.date).map { |date| Work.total_on(date, @works).to_f }
-      @xdata = (@works.first.date..@works.last.date).each.map { |d| d.to_s[8, 2] }
-      @w_avg = current_user.works.get_avg(@start_at, @end_at, @client_id)
-      @w_min = current_user.works.get_min(@start_at, @end_at, @client_id)
-      @w_max = current_user.works.get_max(@start_at, @end_at, @client_id)
-      @w_days = current_user.works.get_days(@start_at, @end_at, @client_id)
+      if @works.empty?
+        flash[:error] = I18n.t("messages.empty_work")
+        redirect_to input_path
+      else
+        @total_std = current_user.works.build_report(@start_at, @end_at, @client_id).sum(:working_hours).to_f
+        @username = current_user.user_name
+        @total_tage = @total_std / 8.0
+        @akt_monat = I18n.t("date.month_names")[@start_at.to_s[6, 2].to_i]
+        @bar_chart= (@works.first.date..@works.last.date).map { |date| Work.total_on(date, @works).to_f }
+        @xdata = (@works.first.date..@works.last.date).each.map { |d| d.to_s[8, 2] }
+        @w_avg = current_user.works.get_avg(@start_at, @end_at, @client_id)
+        @w_min = current_user.works.get_min(@start_at, @end_at, @client_id)
+        @w_max = current_user.works.get_max(@start_at, @end_at, @client_id)
+        @w_days = current_user.works.get_days(@start_at, @end_at, @client_id)
+      end
     end
-
-  end
-
+   end
 end
 
 
