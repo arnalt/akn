170064ab430add389ef7d4ca44fc62d7537117fd
aral <altenhofer@web.de>
2013-05-27 10:22:59 +0200

    work

--- spec/requests/report_pages_spec.rb
+++ spec/requests/report_pages_spec.rb
@@ -18,37 +18,12 @@ describe "Works pages" do
 
   describe "excel" do
     before do
+
       visit excel_path
     end
 
    it { should have_selector('title', text:'Excel Reports') }
   end
 
-  describe "output" do
-    before do
-     # user = FactoryGirl.create(:user)
-     # client = FactoryGirl.create(:client)
-     # task = FactoryGirl.create(:task, :client => client)
-    #  work = FactoryGirl.create(:work, :task => task, :user => user)
-
-     user = User.first
-     client = Client.first
-
-      sign_in user
-      visit input_path
-      fill_in "Client",            with: client.name
-      fill_in "Start_at",         with: Work.start_at
-      fill_in "End_at",            with: Work.work.end_at
-      click_button "GO"
-
-
-
-
-    #  visit output_path({:id => work.id, :clientname => client.name, :period_begin => '01.04.2013', :period_end => '30.04.2013'})
-    end
-
-   it { should have_selector('title', text:'Output Reports') }
-
-  end
 
   end
