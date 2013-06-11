535a7801ae7098ff5053e6e21b2ba1108ac08c9f
aral <altenhofer@web.de>
2013-06-11 17:12:19 +0200

    some layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # Returns the full title on a per-page basis.
  def full_title(page_title)
-     base_title = "AKRA CP Arbeitskostennachweis"
+     base_title = t('text.base_title')
      if page_title.empty?
         base_title
     else
